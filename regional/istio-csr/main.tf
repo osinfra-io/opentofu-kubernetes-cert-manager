7313a0018b809b5390cc4b4669a8953875b0dbbb
Brett Curtis <brett@osinfra.io>
2024-09-28 09:48:39 -0400

    Test previous release

--- regional/istio-csr/main.tf
+++ regional/istio-csr/main.tf
@@ -7,20 +7,20 @@ resource "helm_release" "cert_manager_istio_csr" {
   namespace  = "cert-manager"
   repository = var.chart_repository
 
-  set {
-    name  = "podLabels.tags\\.datadoghq\\.com/env"
-    value = var.environment
-  }
+  # set {
+  #   name  = "podLabels.tags\\.datadoghq\\.com/env"
+  #   value = var.environment
+  # }
 
-  set {
-    name  = "podLabels.tags\\.datadoghq\\.com/version"
-    value = var.cert_manager_istio_csr_version
-  }
+  # set {
+  #   name  = "podLabels.tags\\.datadoghq\\.com/version"
+  #   value = var.cert_manager_istio_csr_version
+  # }
 
-  set {
-    name  = "podLabels.tags\\.datadoghq\\.com/service"
-    value = "cert-manager-istio-csr"
-  }
+  # set {
+  #   name  = "podLabels.tags\\.datadoghq\\.com/service"
+  #   value = "cert-manager-istio-csr"
+  # }
 
   values = [
     file("${path.module}/helm/cert-manager-istio-csr.yml")
