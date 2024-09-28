87799577c8765ec0f7b7fd459ea8c398fc06f621
Brett Curtis <brett@osinfra.io>
2024-09-28 13:39:41 -0400

    Cleanup

--- regional/manifests/README.md
+++ regional/manifests/README.md
@@ -7,7 +7,9 @@ No requirements.
 
 ## Providers
 
-No providers.
+| Name | Version |
+|------|---------|
+| <a name="provider_kubernetes"></a> [kubernetes](#provider\_kubernetes) | 2.31.0 |
 
 ## Modules
 
@@ -15,7 +17,15 @@ No modules.
 
 ## Resources
 
-No resources.
+| Name | Type |
+|------|------|
+| [kubernetes_manifest.istio_ca_certificate](https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs/resources/manifest) | resource |
+| [kubernetes_manifest.istio_ca_issuer](https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs/resources/manifest) | resource |
+| [kubernetes_manifest.istio_gateway_ca_certificate](https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs/resources/manifest) | resource |
+| [kubernetes_manifest.istio_gateway_ca_issuer](https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs/resources/manifest) | resource |
+| [kubernetes_manifest.istio_gateway_selfsigned_issuer](https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs/resources/manifest) | resource |
+| [kubernetes_manifest.istio_gateway_tls](https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs/resources/manifest) | resource |
+| [kubernetes_manifest.istio_selfsigned_issuer](https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs/resources/manifest) | resource |
 
 ## Inputs
 
--- regional/manifests/main.tf
+++ regional/manifests/main.tf
@@ -1,89 +1,173 @@
 # Kubernetes Manifest Resource
 # https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs/resources/manifest
 
-# resource "kubernetes_manifest" "istio_ca_certificate" {
-#   manifest = {
-#     apiVersion = "cert-manager.io/v1"
-#     kind       = "Certificate"
+resource "kubernetes_manifest" "istio_ca_certificate" {
+  manifest = {
+    apiVersion = "cert-manager.io/v1"
+    kind       = "Certificate"
 
-#     metadata = {
-#       name      = "istio-ca"
-#       namespace = "istio-system"
-#     }
+    metadata = {
+      name      = "istio-ca"
+      namespace = "istio-system"
+    }
 
-#     spec = {
-#       commonName = "istio-ca"
-#       duration   = "2160h"
-#       isCA       = true
+    spec = {
+      commonName = "istio-ca"
+      duration   = "2160h"
+      isCA       = true
 
-#       issuerRef = {
-#         name  = "selfsigned"
-#         kind  = "Issuer"
-#         group = "cert-manager.io"
-#       }
+      issuerRef = {
+        name  = "selfsigned"
+        kind  = "Issuer"
+        group = "cert-manager.io"
+      }
 
-#       privateKey = {
-#         algorithm = "ECDSA"
-#         size      = 256
-#       }
+      privateKey = {
+        algorithm = "ECDSA"
+        size      = 256
+      }
 
-#       secretName = "istio-ca"
+      secretName = "istio-ca"
 
-#       subject = {
-#         organizations = ["istio.osinfra.io"]
-#       }
-#     }
-#   }
-# }
+      subject = {
+        organizations = ["istio.osinfra.io"]
+      }
+    }
+  }
+}
 
-# resource "kubernetes_manifest" "istio_ca_issuer" {
-#   manifest = {
-#     apiVersion = "cert-manager.io/v1"
-#     kind       = "Issuer"
+resource "kubernetes_manifest" "istio_ca_issuer" {
+  manifest = {
+    apiVersion = "cert-manager.io/v1"
+    kind       = "Issuer"
 
-#     metadata = {
-#       name      = "istio-ca"
-#       namespace = "istio-system"
-#     }
+    metadata = {
+      name      = "istio-ca"
+      namespace = "istio-system"
+    }
 
-#     spec = {
-#       ca = {
-#         secretName = "istio-ca"
-#       }
-#     }
-#   }
-# }
+    spec = {
+      ca = {
+        secretName = "istio-ca"
+      }
+    }
+  }
+}
 
-# resource "kubernetes_manifest" "ingress_gateway_tls" {
-#   manifest = {
-#     apiVersion = "cert-manager.io/v1"
-#     kind       = "Certificate"
+resource "kubernetes_manifest" "istio_selfsigned_issuer" {
+  manifest = {
+    apiVersion = "cert-manager.io/v1"
+    kind       = "Issuer"
 
-#     metadata = {
-#       name      = "ingress-cert"
-#       namespace = "istio-ingress"
-#     }
+    metadata = {
+      name      = "selfsigned"
+      namespace = "istio-system"
+    }
 
-#     spec = {
-#       secretName = "istio-gateway-tls"
-#       commonName = "istio.osinfra.io"
-#       dnsNames   = ["istio.osinfra.io"]
-#     }
-#   }
-# }
+    spec = {
+      selfSigned = {}
+    }
+  }
+}
 
-# resource "kubernetes_manifest" "selfsigned_issuer" {
-#   manifest = {
-#     apiVersion = "cert-manager.io/v1"
-#     kind       = "Issuer"
+resource "kubernetes_manifest" "istio_gateway_ca_certificate" {
+  manifest = {
+    apiVersion = "cert-manager.io/v1"
+    kind       = "Certificate"
 
-#     metadata = {
-#       name      = "selfsigned"
-#       namespace = "istio-system"
-#     }
+    metadata = {
+      name      = "istio-gateway-ca"
+      namespace = "istio-ingress"
+    }
 
-#     spec = {
-#       selfSigned = {}
-#     }
-#   }
-# }
+    spec = {
+      commonName = "istio-gateway-ca"
+      duration   = "2160h"
+      isCA       = true
+
+      issuerRef = {
+        name  = "selfsigned"
+        kind  = "Issuer"
+        group = "cert-manager.io"
+      }
+
+      privateKey = {
+        algorithm = "ECDSA"
+        size      = 256
+      }
+
+      secretName = "istio-gateway-ca"
+
+      subject = {
+        organizations = ["istio.osinfra.io"]
+      }
+    }
+  }
+}
+
+resource "kubernetes_manifest" "istio_gateway_ca_issuer" {
+  manifest = {
+    apiVersion = "cert-manager.io/v1"
+    kind       = "Issuer"
+
+    metadata = {
+      name      = "istio-gateway-ca"
+      namespace = "istio-ingress"
+    }
+
+    spec = {
+      ca = {
+        secretName = "istio-gateway-ca"
+      }
+    }
+  }
+}
+
+resource "kubernetes_manifest" "istio_gateway_tls" {
+  manifest = {
+    apiVersion = "cert-manager.io/v1"
+    kind       = "Certificate"
+
+    metadata = {
+      name      = "istio-gateway-tls"
+      namespace = "istio-ingress"
+    }
+
+    spec = {
+      commonName = "istio-gateway.osinfra.io"
+      dnsNames   = ["*"]
+      duration   = "2160h"
+      isCA       = false
+
+      issuerRef = {
+        name  = "istio-gateway-ca"
+        kind  = "Issuer"
+        group = "cert-manager.io"
+      }
+
+      renewBefore = "360h"
+      secretName  = "istio-gateway-tls"
+
+      usages = [
+        "client auth",
+        "server auth"
+      ]
+    }
+  }
+}
+
+resource "kubernetes_manifest" "istio_gateway_selfsigned_issuer" {
+  manifest = {
+    apiVersion = "cert-manager.io/v1"
+    kind       = "Issuer"
+
+    metadata = {
+      name      = "selfsigned"
+      namespace = "istio-ingress"
+    }
+
+    spec = {
+      selfSigned = {}
+    }
+  }
+}
