3f0ed78b2633dbfed2a1864cacc2a1b8da2ed3f1
Brett Curtis <brett@osinfra.io>
2024-10-12 12:01:17 -0400

    Support for Open Policy Agent Gatekeeper (#4)
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- regional/istio-csr/variables.tf
+++ regional/istio-csr/variables.tf
@@ -24,10 +24,39 @@ variable "environment" {
   }
 }
 
+variable "cluster_id" {
+  description = "The cluster ID to use for the Istio CSR"
+  type        = string
+}
+
 variable "cert_manager_istio_csr_version" {
   description = "The version to install for the Istio CSR, this is used for the chart as well as the image tag"
   type        = string
   default     = "0.12.0"
+}
+
+variable "resources_limits_cpu" {
+  description = "The CPU limit for the Istio CSR container"
+  type        = string
+  default     = "50m"
+}
+
+variable "resources_limits_memory" {
+  description = "The memory limit for the Istio CSR container"
+  type        = string
+  default     = "64Mi"
+}
+
+variable "resources_requests_cpu" {
+  description = "The CPU request for the Istio CSR container"
+  type        = string
+  default     = "25m"
+}
+
+variable "resources_requests_memory" {
+  description = "The memory request for the Istio CSR container"
+  type        = string
+  default     = "32Mi"
 }
 
 variable "node_location" {
