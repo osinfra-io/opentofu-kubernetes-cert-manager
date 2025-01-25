e7d1c75b10751b3ff5e9f8073d7d9f136ae50874
Brett Curtis <brett@osinfra.io>
2025-01-25 09:18:04 -0500

    Comment out Datadog pod labels in locals.tf

--- regional/istio-csr/locals.tf
+++ regional/istio-csr/locals.tf
@@ -5,12 +5,12 @@ locals {
   cluster_id = module.helpers.zone != null ? "${var.cluster_prefix}-${module.helpers.region}-${module.helpers.zone}-${module.helpers.env}" : "${var.cluster_prefix}-${module.helpers.region}-${module.helpers.env}"
 
   helm_values = {
-    "app.server.clusterID"                     = local.cluster_id
-    "podLabels.tags\\.datadoghq\\.com/env"     = module.helpers.environment
-    "podLabels.tags\\.datadoghq\\.com/version" = var.cert_manager_istio_csr_version
-    "resources.limits.cpu"                     = var.resources_limits_cpu
-    "resources.limits.memory"                  = var.resources_limits_memory
-    "resources.requests.cpu"                   = var.resources_requests_cpu
-    "resources.requests.memory"                = var.resources_requests_memory
+    "app.server.clusterID" = local.cluster_id
+    #"podLabels.tags\\.datadoghq\\.com/env"     = module.helpers.environment
+    #"podLabels.tags\\.datadoghq\\.com/version" = var.cert_manager_istio_csr_version
+    "resources.limits.cpu"      = var.resources_limits_cpu
+    "resources.limits.memory"   = var.resources_limits_memory
+    "resources.requests.cpu"    = var.resources_requests_cpu
+    "resources.requests.memory" = var.resources_requests_memory
   }
 }
