2bcb315d16a4174a7bdb1f6ff3872c1399e66d46
Brett Curtis <brett@osinfra.io>
2025-02-01 19:44:25 -0500

    Base64 encode self-signed certificate and private key outputs for cert-manager root certificate

--- outputs.tf
+++ outputs.tf
@@ -3,11 +3,11 @@
 
 output "tls_self_signed_cert_cert_manager_root_cert" {
   description = "The self-signed certificate for the cert-manager root certificate"
-  value       = tls_self_signed_cert.cert_manager_root.cert_pem
+  value       = base64encode(tls_self_signed_cert.cert_manager_root.cert_pem)
 }
 
 output "tls_self_signed_cert_cert_manager_root_key" {
   description = "The private key for the cert-manager root certificate"
-  value       = tls_self_signed_cert.cert_manager_root.private_key_pem
+  value       = base64encode(tls_self_signed_cert.cert_manager_root.private_key_pem)
   sensitive   = true
 }
